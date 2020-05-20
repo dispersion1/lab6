77654fcd56ce779680ff8ad3e31c48f268c654d7
User <User@PC-LITE>
2020-05-20 18:06:29 +0300

    Добавьте файлы проекта.

---
+++ lab6/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение параметра x: ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = 3 * Math.Sin(x) * (x + 2);
+                Console.WriteLine($"Значение F = {F}.");
+            Console.ReadKey();
+        }
+    }
+}
